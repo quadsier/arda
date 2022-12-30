631a7e38781fe93bff1400f7442160b30a448769
quadsier <gordon_gungor@hotmail.com>
2022-12-30 17:06:55 +0300

    ardaaa

--- SR3_pytorchnoload.py
+++ SR3_pytorchnoload.py
@@ -493,7 +493,7 @@ if __name__ == "__main__":
     LR_size = 32
     img_size = 128
     root = './images'
-    testroot = './images'
+    testroot = './test'
 
     transforms_ = transforms.Compose([transforms.Resize(img_size), transforms.ToTensor(), 
                                         transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
